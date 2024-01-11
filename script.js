b8985343d90501d4763c06acb9066dce37279950
Shem <shemenekok@gmail.com>
2024-01-11 17:30:31 +0200

    Created the new variable for output

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let display = document.querySelector(".display");
-let buttons = document.querySelectorAll(".button");
- let Chars
-//funtion to calculatte based on button that is clicked
+let buttons = document.querySelectorAll(".buttons");
+let splChars = ["%", "*", "/", "+", "="];
+let output = "";
+
+// Function to calculate based on the button that is clicked
 let calculate = (btnValue) => {
-  comsole.log(btnValue);
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "*0.01"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && splChars.includes(btnValue)) return;
+
+    output += btnValue;
+  }
+
+  display.value = output;
 };
 
-//console.log(display, buttons);
-
-//Event Listener for buttons , call calculate() on click.
-
+// Event Listener for buttons, call calculate() on click.
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => console.lof(e.target.dataset.value));
+  button.addEventListener("click", (e) => calculate(e.target.innerText));
 });
